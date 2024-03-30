1cb6ffd28a6752b5f9a8ae4bb48dcc0a136c2974
thande-zap <devanshbatra23@gmail.com>
2024-03-30 22:31:02 +0530

    commit

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-source = requests.get("")
+# source = requests.get("")
 
 
-with open("product-list.json", "r") as f:
+with open("db/product-list.json", "r") as f:
     data = json.load(f)
 
 data = data["searchData"]["results"]["products"]
 
 data = {product["productId"]: product["productName"] for product in data}
 
-with open("product.json", "w") as f:
+with open("db/product.json", "w") as f:
     json.dump(data, f)
